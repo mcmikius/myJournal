c126fe859ed8259b8471483220e9fcd5cfd28331
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-30 20:45:54 +0300

    add Leaf

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,21 +1,29 @@
 import FluentSQLite
 import Vapor
+import Leaf
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
     try services.register(FluentSQLiteProvider())
+    // Register Leaf templating engine
+    try services.register(LeafProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
 
+    
+    
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
+    
+    
+        
 
     // Configure a SQLite database
     let sqlite = try SQLiteDatabase(storage: .memory)
@@ -29,4 +37,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var migrations = MigrationConfig()
 //    migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
+    
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 }
